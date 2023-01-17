8e0cdcd08c62c366e3f98613b1366d1d33ec25b4
ferdyrimando <ferdie.rimando@gmail.com>
2023-01-17 11:19:01 +0800

    Add files via upload

--- db_admin.sql
+++ db_admin.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2020 at 07:00 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: Jan 17, 2023 at 04:17 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbl_roles` (
   `id` int(11) NOT NULL COMMENT 'role_id',
   `role` varchar(255) DEFAULT NULL COMMENT 'role_text'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_roles`
@@ -55,26 +54,29 @@ CREATE TABLE `tbl_users` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `mobile` varchar(25) DEFAULT NULL,
+  `birthdate` datetime(6) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `department` varchar(10) DEFAULT NULL,
+  `sss_no` int(15) DEFAULT NULL,
+  `tin_no` int(15) DEFAULT NULL,
+  `philhealth_no` int(15) DEFAULT NULL,
   `roleid` tinyint(4) DEFAULT NULL,
   `isActive` tinyint(4) DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`id`, `name`, `username`, `email`, `password`, `mobile`, `roleid`, `isActive`, `created_at`, `updated_at`) VALUES
-(7, 'Nababur', 'Nababurbd', 'nababurbd@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 1, 0, '2020-03-12 16:23:01', '2020-03-12 16:23:01'),
-(12, 'Rayhan', 'Rayhan', 'rayhankabir@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-12 18:20:24', '2020-03-12 18:20:24'),
-(15, 'Sanjia Akther', 'Sanjida', 'sanjida@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-12 19:32:27', '2020-03-12 19:32:27'),
-(16, 'Abid Ali', 'Abid', 'abid@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 3, 0, '2020-03-13 05:08:26', '2020-03-13 05:08:26'),
-(17, 'Abdur Rouf', 'Rouf', 'rouf@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:08:53', '2020-03-13 05:08:53'),
-(18, 'Maruf Jaman', 'Maruf', 'maruf@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 0, '2020-03-13 05:09:18', '2020-03-13 05:09:18'),
-(19, 'Humayun ', 'Munna', 'munna@gmail.com', '66c3241204bea40578eb993f41f7c4b1ab982dab', '01717090233', 3, 0, '2020-03-13 05:09:49', '2020-03-13 05:09:49'),
-(20, 'Rased ', 'Rashed', 'rashed@gmail.com', '188000e1f0fb4075ae1c659697b96296f982cdc4', '01717090233', 2, 1, '2020-03-13 05:10:24', '2020-03-13 05:10:24'),
-(21, 'Millon ', 'Millon', 'millon@gmail.com', '05c19fb114728eabf85f47c858914ca42ddd2dae', '01717090233', 1, 1, '2020-03-13 05:11:02', '2020-03-13 05:11:02');
+INSERT INTO `tbl_users` (`id`, `name`, `username`, `email`, `password`, `mobile`, `birthdate`, `gender`, `address`, `department`, `sss_no`, `tin_no`, `philhealth_no`, `roleid`, `isActive`, `created_at`, `updated_at`) VALUES
+(23, 'christian', 'christian', 'christian@ymail.com', '93e72eb3cc2a4bf24508775dc9c69814c3d70536', '09661234567', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, 0, '2023-01-17 00:04:15', '2023-01-17 00:04:15'),
+(24, 'Ferdinand', 'ferdyrimando', 'ferdyrimando@ymail.com', 'f3rdinand', '09684122227', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, '2023-01-17 01:12:03', '2023-01-17 01:12:03'),
+(26, 'justin', 'justin', 'justin@gmail.com', 'just1n', '099100000', '2023-01-17 10:14:18.000000', 'male', 'San Fernando City, La Union', 'ITSM', 1111111, 1111111, 1111111, 3, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(27, 'daryl', 'daryl', 'daryl@gmail.com', '1200b854b2e5ec1e74428eb6c2a7c9a36569ce9e', '09122222', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, 0, '2023-01-17 02:52:43', '2023-01-17 02:52:43'),
+(28, 'michael', 'michael', 'michael@gmail.com', '7e64081df13b56a7adf768d69ab79e1d77a9a6c8', '09222222', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, '2023-01-17 02:55:45', '2023-01-17 02:55:45');
 
 --
 -- Indexes for dumped tables
@@ -106,7 +108,7 @@ ALTER TABLE `tbl_roles`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
